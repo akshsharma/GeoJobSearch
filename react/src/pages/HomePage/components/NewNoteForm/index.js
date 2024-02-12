fe62fb7afad1d41431f0e5eff00d39c71b5b95f4
Jessica Nguyen <jnguye27@uoguelph.ca>
2024-02-11 22:47:37 -0500

    Testing .css

--- react/src/pages/HomePage/components/NewNoteForm/index.js
+++ react/src/pages/HomePage/components/NewNoteForm/index.js
@@ -2,15 +2,17 @@ import Stack from 'react-bootstrap/Stack';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
+import './NewNoteForm.css';
+
 const NewNoteForm = (props) => {
-    return <div>
+    return <div className="NewNoteForm">
         <Row>
-            <h2>New Note</h2>
+            <h2>Post a Job</h2>
         </Row>
         <Row>
         <Stack direction="horizontal" gap={3}>
-            <Form.Control as="textarea" rows={3} placeholder="Note..." value={props.value} onChange={(e) => props.onChange(e.target.value)}/>
-            <Button onClick={() => props.onSubmit()}>Add Note</Button>
+            <Form.Control as="textarea" rows={3} placeholder="Input Job Information Here" value={props.value} onChange={(e) => props.onChange(e.target.value)}/>
+            <Button onClick={() => props.onSubmit()}>Submit Job Posting</Button>
         </Stack>
         </Row>
     </div>
