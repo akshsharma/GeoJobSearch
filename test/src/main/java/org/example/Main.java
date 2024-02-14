57cbad3ad0ca236f43755d8e18f1c065ef0c1b8a
unknown <gshresth@uoguelph.ca>
2024-02-14 18:51:30 -0500

    added test folder for junit tests

---
+++ test/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        System.out.println(x);
+    }
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
