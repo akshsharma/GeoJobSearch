35b9db778cb53bff943c5fda3b67fbb67a3eff35
Tyler Janvrin <tjanvrin@uoguelph.ca>
2024-02-24 15:49:17 -0500

    Fixed bug where data wasn't being displayed on the frontend. Had something to do with the old notes app code being present in the react frontend

--- react/src/pages/HomePage/index.js
+++ react/src/pages/HomePage/index.js
@@ -11,19 +11,19 @@ import './HomePage.css';
 
 const HomePage = () => {
     const [jobs, setJobs] = useState([]);
-    const [notes, setNotes] = useState([]);
-    const [count, setCount] = useState(0);
-    const [newNote, setNewNote] = useState('');
-    const [showEdit, setShowEdit] = useState(false);
-    const [noteToEdit, setNoteToEdit] = useState(null);
+    // const [notes, setNotes] = useState([]);
+    // const [count, setCount] = useState(0);
+    // const [newNote, setNewNote] = useState('');
+    // const [showEdit, setShowEdit] = useState(false);
+    // const [noteToEdit, setNoteToEdit] = useState(null);
     const [loading, setLoading] = useState(false);
     const [loadingCount, setLoadingCount] = useState(false);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
 
     useEffect(() => {
-        fetchCount();
-        fetchAllNotes();
+        // fetchCount();
+        // fetchAllNotes();
         fetchJobList();
     }, [])
 
@@ -50,98 +50,100 @@ const HomePage = () => {
             });
     }
 
-    const fetchAllNotes = () => {
-        setLoading(true);
-        fetch('/api/notes/all')
-            .then(status)
-            .then(res => res.json())
-            .then(data => {
-                setNotes(data);
-                setLoading(false);
-            }).catch(error => {
-                setErrorMessage(error.message);
-                setError(true);
-            });
-    }
+    // was getting errors that referenced these lines, so I commented them out - Tyler
 
-    const fetchCount = () => {
-        setLoadingCount(true);
-        fetch('/api/notes/count')
-            .then(status)
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .then(data => {
-                setCount(data);
-                setLoadingCount(false);
-            }).catch(error => {
-                setErrorMessage(error.message);
-                setError(true);
-            });
-    }
+    // const fetchAllNotes = () => {
+    //     setLoading(true);
+    //     fetch('/api/notes/all')
+    //         .then(status)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setNotes(data);
+    //             setLoading(false);
+    //         }).catch(error => {
+    //             setErrorMessage(error.message);
+    //             setError(true);
+    //         });
+    // }
 
-    const addNewNote = () => {
-        fetch('/api/notes/add', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                text: newNote
-            })
-        })
-        .then(status)
-        .then(res => {
-            console.log(res);
-            setNewNote('');
-            fetchAllNotes();
-            fetchCount();
-        }).catch(error => {
-            setErrorMessage(error.message);
-            setError(true);
-        });
-    }
+    // const fetchCount = () => {
+    //     setLoadingCount(true);
+    //     fetch('/api/notes/count')
+    //         .then(status)
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    //         .then(data => {
+    //             setCount(data);
+    //             setLoadingCount(false);
+    //         }).catch(error => {
+    //             setErrorMessage(error.message);
+    //             setError(true);
+    //         });
+    // }
 
-    const deleteNote = (id) => {
-        fetch(`/api/notes/delete/${id}`, {
-            method: 'DELETE'
-        })
-        .then(status)
-        .then(res => {
-            console.log(res);
-            fetchAllNotes();
-            fetchCount();
-        }).catch(error => {
-            setErrorMessage(error.message);
-            setError(true);
-        });
-    }
+    // const addNewNote = () => {
+    //     fetch('/api/notes/add', {
+    //         method: 'POST',
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //             text: newNote
+    //         })
+    //     })
+    //     .then(status)
+    //     .then(res => {
+    //         console.log(res);
+    //         setNewNote('');
+    //         fetchAllNotes();
+    //         fetchCount();
+    //     }).catch(error => {
+    //         setErrorMessage(error.message);
+    //         setError(true);
+    //     });
+    // }
 
-    const triggerEditModal = (note) => {
-        setNoteToEdit(note);
-        setShowEdit(true);
-    }
+    // const deleteNote = (id) => {
+    //     fetch(`/api/notes/delete/${id}`, {
+    //         method: 'DELETE'
+    //     })
+    //     .then(status)
+    //     .then(res => {
+    //         console.log(res);
+    //         fetchAllNotes();
+    //         fetchCount();
+    //     }).catch(error => {
+    //         setErrorMessage(error.message);
+    //         setError(true);
+    //     });
+    // }
 
-    const editClose = () => {
-        setShowEdit(false);
-    }
+    // const triggerEditModal = (note) => {
+    //     setNoteToEdit(note);
+    //     setShowEdit(true);
+    // }
 
-    const editNote = (note) => {
-        fetch('/api/notes/update', {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(note)
-        })
-        .then(status)
-        .then(res => {
-            console.log(res);
-            fetchAllNotes();
-        }).catch(error => {
-            setErrorMessage(error.message);
-            setError(true);
-        });
-    }
+    // const editClose = () => {
+    //     setShowEdit(false);
+    // }
+
+    // const editNote = (note) => {
+    //     fetch('/api/notes/update', {
+    //         method: 'PUT',
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(note)
+    //     })
+    //     .then(status)
+    //     .then(res => {
+    //         console.log(res);
+    //         fetchAllNotes();
+    //     }).catch(error => {
+    //         setErrorMessage(error.message);
+    //         setError(true);
+    //     });
+    // }
 
     return(
         <div>
