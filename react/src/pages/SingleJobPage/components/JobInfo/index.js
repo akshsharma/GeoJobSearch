36a5701dd6d2424624e307af8b1c60180e9243ee
Markus Bourdon <bourdon@uoguelph.ca>
2024-03-21 01:20:27 -0400

    fixed eployer, usestate and useeffect not being declared

--- react/src/pages/SingleJobPage/components/JobInfo/index.js
+++ react/src/pages/SingleJobPage/components/JobInfo/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
@@ -27,7 +27,6 @@ const JobInfo = () => {
         <div><b>Location:</b> {job.job_location}</div>
         <div><b>Salary min:</b> ${job.job_salary_min}</div>
         <div><b>Salary max:</b> ${job.job_salary_max}</div>
-        <div><b>Employer:</b> {employer.employer_name}</div>
     </div>;
 
 };
