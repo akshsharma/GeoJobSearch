3778853ed628f93bc265f8aab036461433ef4bda
Markus Bourdon <bourdon@uoguelph.ca>
2024-03-21 02:58:37 -0400

    in theory this should work, for some reaon endpoint is returning the index.html file insteaed of the job info even though it looks fine in swagger ui

--- react/src/pages/SingleJobPage/components/JobInfo/index.js
+++ react/src/pages/SingleJobPage/components/JobInfo/index.js
@@ -1,32 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 
-const JobInfo = () => {
-
-    const [job, setjob] = useState(null);
-
-    /* fetch data when component mounts */
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    /* fetch data from endpoint */
-    const fetchData = async () => {
-        try {
-            const response = await axios.get('api/jobs/{id}'); 
-            setjob(response);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+const JobInfo = (job) => {
 
     return <div className="geoJobSearch-align">
-        <div><b>Title:</b> {job.job_title}</div>
-        <div><b>Description:</b> {job.job_description}</div>
-        <div><b>Location:</b> {job.job_location}</div>
-        <div><b>Salary min:</b> ${job.job_salary_min}</div>
-        <div><b>Salary max:</b> ${job.job_salary_max}</div>
+        <div><h3>{job.title}</h3></div>
+        <div><p>{job.description}</p></div>
+        <div><b>Location:</b> {job.location}</div>
+        <div><b>Salary min:</b> ${job.salaryMin}</div>
+        <div><b>Salary max:</b> ${job.salaryMax}</div>
     </div>;
 
 };
--- react/src/pages/SingleJobPage/index.js
+++ react/src/pages/SingleJobPage/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import ErrorAlert from '../../components/ErrorAlert';
 import LoadingSpinner from '../../components/LoadingSpinner';
@@ -7,13 +8,53 @@ import './SingleJobPage.css';
 import JobInfo from './components/JobInfo';
 
 const SingleJobPage = () => {
+
+    const { id } = useParams();
+
+    console.log(id);
+
+    const [job, setJob] = useState([]);
     const [loading, setLoading] = useState(false);
     const [loadingCount] = useState(false);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
 
+    useEffect(() => {
+        fetchJob();
+    }, []);
 
+    const status = (res) => {
+        if (!res.ok) {
+            throw new Error('Something Went Wrong');
+        }
+        return res;
+    };
     
+    const fetchJob = async () => {
+        // setLoading(true);
+        // fetch('api/jobs/1')
+        //     .then(status)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         setJob(data);
+        //         setLoading(false);
+        //     }).catch(error => {
+        //         setErrorMessage(error.message);
+        //         setError(true);
+        //     });
+        try {
+            const response = await fetch(`api/jobs/${id}`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const data = await response.json();
+            console.log(data); // Use the fetched data as needed
+        } catch (error) {
+            console.error('Error fetching job data:', error);
+        }
+    };
+
     return(
         <div>
             <GlobalNavBar pageName='Job Info'/>
@@ -28,7 +69,7 @@ const SingleJobPage = () => {
                     :
                     <div>
                         <div className="geoJobSearch-JobInfo">
-                            <JobInfo/>
+                            <JobInfo entry={job}/>
                         </div>
                     </div>
                 }
